d19634c1c8c3847fdeed3395234979d03c498bdb
Kren19 <86132935+Kren19@users.noreply.github.com>
2024-06-24 23:09:05 -0500

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,40 @@
 const path = require('path');
+const webpack = require('webpack');
 
 module.exports = {
-  mode: 'development', // Cambia a 'production' en producción
-  entry: './server.js', // Archivo principal de entrada de tu aplicación
+  mode: 'production', // Cambia a 'production' en producción
+  entry: './server.js', // Nombre del archivo principal de entrada
   output: {
     filename: 'bundle.js', // Nombre del archivo de salida
     path: path.resolve(__dirname, 'dist'), // Carpeta donde se guardarán los archivos compilados
   },
-  module: {
-    rules: [
-      {
-        test: /\.css$/,
-        use: ['style-loader', 'css-loader'], // Añade loaders para procesar archivos CSS si los tienes
-      },
-    ],
+  resolve: {
+    fallback: {
+      http: require.resolve('stream-http'),
+      https: require.resolve('https-browserify'),
+      zlib: require.resolve('browserify-zlib'),
+      assert: require.resolve('assert'),
+      util: require.resolve('util'),
+      fs: false, // No incluir fs para el navegador
+      path: false, // No incluir path para el navegador
+      os: false, // No incluir os para el navegador
+      stream: require.resolve('stream-browserify'),
+      crypto: require.resolve('crypto-browserify'),
+      buffer: require.resolve('buffer/'),
+      querystring: require.resolve('querystring-es3'),
+      punycode: require.resolve('punycode'),
+      process: require.resolve('process/browser'),
+      assert: require.resolve('assert/'),
+      events: require.resolve('events/'),
+      constants: require.resolve('constants-browserify'),
+      util: require.resolve('util/'),
+      'async_hooks': require.resolve('async_hooks/'),
+      'string_decoder': require.resolve('string_decoder/'),
+    },
   },
+  plugins: [
+    new webpack.ProvidePlugin({
+      process: 'process/browser',
+    }),
+  ],
 };
