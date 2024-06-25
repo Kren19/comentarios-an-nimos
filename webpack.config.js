699e158af8c373a407a20c106029a51898ef71e5
Kren19 <86132935+Kren19@users.noreply.github.com>
2024-06-24 22:48:39 -0500

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -2,9 +2,17 @@ const path = require('path');
 
 module.exports = {
   mode: 'development', // Cambia a 'production' en producción
-  entry: './server.js', // Nombre del archivo principal de entrada
+  entry: './server.js', // Archivo principal de entrada de tu aplicación
   output: {
     filename: 'bundle.js', // Nombre del archivo de salida
     path: path.resolve(__dirname, 'dist'), // Carpeta donde se guardarán los archivos compilados
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'], // Añade loaders para procesar archivos CSS si los tienes
+      },
+    ],
   },
 };
